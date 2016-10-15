87fa4ddbca0eab1e3e09850d0661b1efaa68d6b3
yaya1721 <Sonyahyun.hs@gmail.com>
2016-10-15 16:44:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ var bgImg= document.createElement("img");
 bgImg.src= "images/map.png";
 var canvas = document.getElementByld("game-canvas");
 var ctx= canvas.getContext("2d");
+function draw(){
+  //將背景圖片畫在canvas上的(0,0)位置
+  ctx.drawImage(bgImg,0,0);
+}
+
+//執行draw函式
+draw();
