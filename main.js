00455c4581b071df103d094eb263944029f07757
yaya1721 <Sonyahyun.hs@gmail.com>
2016-10-15 16:41:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var bgImg= document.createElement("img");
-bgImg.src= "images/mag.png";
+bgImg.src= "images/map.png";
 var canvas = document.getElementByld("game-canvas");
 var ctx= canvas.getContext("2d");
