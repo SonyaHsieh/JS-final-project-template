0a0b434ee66ee257c590460b90a22b15ce2d28db
yaya1721 <Sonyahyun.hs@gmail.com>
2016-10-22 17:20:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function draw(){
   //將背景圖片畫在canvas上的(0,0)位置
   ctx.drawImage(bgImg,0,0,640,480);
   ctx.drawImage(slimeImg,slime.x,slime.y);
-  ctx.drawImage(towerBtn,btn.x,btn.y,);
+  ctx.drawImage(towerBtn,btn.x,btn.y);
   ctx.drawImage(towerImg,cursor.x,cursor.y);
 }
 
