763d279494fc0772f4e375da60f41da06d03911a
yaya1721 <Sonyahyun.hs@gmail.com>
2016-11-19 18:10:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight)
 //瞄準敵人 
 var tower={
  range:96,
- aimingEnemyId= null,
+ aimingEnemyId: null,
  searchEnemy:function(){
  for(var i=0; i<enemies.length; i++){
  var distance = Math.sqrt(Math.pow(this.x-enemies[i].x,2) + Math.pow(this.y-enemies[i].y,2));
